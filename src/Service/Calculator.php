0d478c91c9d4ef49260ead87878a380cd5110492
Berenger Desgardin <berenger.desgardin@gmail.com>
2019-06-24 14:47:57 +0200

    php unit

---
+++ src/Service/Calculator.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: beren
+ * Date: 24/06/2019
+ * Time: 14:30
+ */
+
+namespace App\Service;
+
+class Calculator
+{
+    public function add(int $firstNumber, int $secondNumber): int
+    {
+        return $firstNumber + $secondNumber;
+    }
+
+    public function divide(float $a, float $b): float
+    {
+        if ($b === 0) {
+            throw new InvalidArgumentException('Dividing by zero is impossible🤯🤯🤯🤯🤯');
+        }
+
+        return $a / $b;
+    }
+}
