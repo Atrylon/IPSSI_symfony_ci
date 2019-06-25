0d6ebffba77a69ec3c9a4b797b8ba75d0e019f19
Berenger Desgardin <berenger.desgardin@gmail.com>
2019-06-25 16:59:34 +0200

    test with pull request

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,6 +10,6 @@ class HomeController
 {
     public function handle(): Response
     {
-        return new Response('NOT OK AT ALL IT\'S  VERY TOO HOT');
+        return new Response('NOT OK AT ALL IT\'S  VERY TOO HOT.... TEST WITH PULL REQUEST');
     }
 }
