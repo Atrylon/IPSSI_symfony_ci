4a66a091fdaa8c436619aa1e15ace2b7a7ab5aa9
Berenger Desgardin <berenger.desgardin@gmail.com>
2019-06-28 08:59:48 +0200

    test with travis org

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,6 +10,6 @@ class HomeController
 {
     public function handle(): Response
     {
-        return new Response('NOT OK AT ALL IT\'S  VERY TOO HOT.... TEST WITH PULL REQUEST... NOT ON TRAVIS ANYMORE ?!');
+        return new Response('NOT OK AT ALL IT\'S  VERY TOO HOT.... TEST WITH PULL REQUEST... NOT ON TRAVIS ANYMORE ?! TEST WITH TRAVIS ORG');
     }
 }
